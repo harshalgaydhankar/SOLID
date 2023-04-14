429d9d9faca5769124c19d724085f4a2c893fdd5
Harshal <harshalgaydhankar@gmail.com>
2023-04-14 13:36:38 +0530

    add openclosed exapme

---
+++ src/main/java/com/principle/openClosed/after/Subscriber.java
@@ -0,0 +1,47 @@
+package com.principle.openClosed.after;
+
+
+public abstract class Subscriber {
+
+    Long subscriberId;
+
+    String address;
+
+    Long phoneNumber;
+
+    int baseRate;
+
+    abstract public double calculateBill();
+
+    public Long getSubscriberId() {
+        return subscriberId;
+    }
+
+    public void setSubscriberId(Long subscriberId) {
+        this.subscriberId = subscriberId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBaseRate() {
+        return baseRate;
+    }
+
+    public void setBaseRate(int baseRate) {
+        this.baseRate = baseRate;
+    }
+}
